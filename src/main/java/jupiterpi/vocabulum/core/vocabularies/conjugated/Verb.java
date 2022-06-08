48063a3e8535eef59744771514d864a95537005e
JupiterPi <jupiterpi@outlook.de>
2022-06-08 22:13:10 +0200

    WordbaseVerbs (untested)

--- src/main/java/jupiterpi/vocabulum/core/vocabularies/conjugated/Verb.java
+++ src/main/java/jupiterpi/vocabulum/core/vocabularies/conjugated/Verb.java
@@ -1,7 +1,7 @@
 package jupiterpi.vocabulum.core.vocabularies.conjugated;
 
 import jupiterpi.vocabulum.core.vocabularies.Vocabulary;
-import jupiterpi.vocabulum.core.vocabularies.conjugated.form.VerbForm;
+import jupiterpi.vocabulum.core.vocabularies.conjugated.form.*;
 import org.bson.Document;
 
 public abstract class Verb extends Vocabulary {
@@ -12,9 +12,37 @@ public abstract class Verb extends Vocabulary {
         return Kind.VERB;
     }
 
+    public abstract String getBaseForm();
+
     @Override
     public Document generateWordbaseEntry() {
-        return null;
-        //TODO implement
+        Document document = new Document();
+        document.put("kind", "verb");
+        document.put("base_form", getBaseForm());
+
+        Document formsDocument = new Document();
+        for (Voice voice : Voice.values()) {
+            Document voiceDocument = new Document();
+            for (Tense tense : Tense.values()) {
+                Document tenseDocument = new Document();
+                for (Mode mode : Mode.values()) {
+                    Document modeDocument = new Document();
+                    for (CNumber number : CNumber.values()) {
+                        Document numberDocument = new Document();
+                        for (Person person : Person.values()) {
+                            String form = makeForm(new VerbForm(new ConjugatedForm(person, number), mode, tense, voice));
+                            numberDocument.put(person.toString().toLowerCase(), form);
+                        }
+                        modeDocument.put(number.toString().toLowerCase(), numberDocument);
+                    }
+                    tenseDocument.put(mode.toString().toLowerCase(), modeDocument);
+                }
+                voiceDocument.put(tense.toString().toLowerCase(), tenseDocument);
+            }
+            formsDocument.put(voice.toString().toLowerCase(), voiceDocument);
+        }
+
+        document.put("forms", formsDocument);
+        return document;
     }
 }
---
+++ src/main/java/jupiterpi/vocabulum/core/vocabularies/conjugated/WordbaseVerb.java
@@ -0,0 +1,50 @@
+package jupiterpi.vocabulum.core.vocabularies.conjugated;
+
+import jupiterpi.vocabulum.core.vocabularies.conjugated.form.*;
+import jupiterpi.vocabulum.core.vocabularies.conjugated.schemas.FormInfo;
+import org.bson.Document;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordbaseVerb extends Verb {
+    private String baseForm;
+    private Map<VerbForm, String> forms;
+
+    public WordbaseVerb(String baseForm, Document forms) {
+        this.baseForm = baseForm;
+
+        this.forms = new HashMap<>();
+        for (Voice voice : Voice.values()) {
+            Document voiceDocument = (Document) forms.get(voice.toString().toLowerCase());
+            for (Tense tense : Tense.values()) {
+                Document tenseDocument = (Document) voiceDocument.get(tense.toString().toLowerCase());
+                FormInfo.Root root = FormInfo.Root.valueOf(tenseDocument.getString("root").toUpperCase());
+                for (Mode mode : Mode.values()) {
+                    Document modeDocument = (Document) tenseDocument.get(mode.toString().toLowerCase());
+                    for (CNumber number : CNumber.values()) {
+                        Document numberDocument = (Document) modeDocument.get(number.toString().toLowerCase());
+                        for (Person person : Person.values()) {
+                            VerbForm form = new VerbForm(new ConjugatedForm(person, number), mode, tense, voice);
+                            this.forms.put(form, numberDocument.getString(person.toString().toLowerCase()));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static WordbaseVerb readFromDocument(Document document) {
+        return new WordbaseVerb(document.getString("base_form"), (Document) document.get("forms"));
+    }
+
+    @Override
+    public String getBaseForm() {
+        return baseForm;
+    }
+
+    @Override
+    public String makeForm(VerbForm form) {
+        return forms.get(form);
+    }
+}
