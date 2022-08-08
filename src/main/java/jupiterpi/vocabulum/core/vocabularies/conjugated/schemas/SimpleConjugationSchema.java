b9d8eb1079511824c05f7ae1f5cc7815916237b3
JupiterPi <jupiterpi@outlook.de>
2022-08-08 16:37:44 +0200

    SimpleConjugationSchema now correctly reads with infinitive forms

--- src/main/java/jupiterpi/vocabulum/core/vocabularies/conjugated/schemas/SimpleConjugationSchema.java
+++ src/main/java/jupiterpi/vocabulum/core/vocabularies/conjugated/schemas/SimpleConjugationSchema.java
@@ -13,26 +13,35 @@ public class SimpleConjugationSchema extends ConjugationSchema {
         String name = document.getString("name");
         SimpleConjugationSchema schema = new SimpleConjugationSchema(name);
 
-        try {
+        // Kind.INFINITIVE
+        Document infinitiveFormsDocument = (Document) document.get("infinitive");
+        for (InfinitiveTense infinitiveTense : InfinitiveTense.values()) {
+            Document infinitiveTenseDocument = (Document) infinitiveFormsDocument.get(infinitiveTense.toString().toLowerCase());
             for (Voice voice : Voice.values()) {
-                Document voiceDocument = (Document) document.get(voice.toString().toLowerCase());
-                for (Tense tense : Tense.values()) {
-                    Document tenseDocument = (Document) voiceDocument.get(tense.toString().toLowerCase());
-                    for (Mode mode : Mode.values()) {
-                        Document modeDocument = (Document) tenseDocument.get(mode.toString().toLowerCase());
-                        for (CNumber number : CNumber.values()) {
-                            Document numberDocument = (Document) modeDocument.get(number.toString().toLowerCase());
-                            for (Person person : Person.values()) {
-                                String pattern = numberDocument.getString(person.toString().toLowerCase());
-                                VerbForm form = new VerbForm(new ConjugatedForm(person, number), mode, tense, voice);
-                                schema.patterns.put(form, Pattern.fromString(pattern));
-                            }
+                String pattern = infinitiveTenseDocument.getString(voice.toString().toLowerCase());
+                VerbForm form = new VerbForm(infinitiveTense, voice);
+                schema.patterns.put(form, Pattern.fromString(pattern));
+            }
+        }
+
+        // Kind.BASIC
+        Document basicFormsDocument = (Document) document.get("basic");
+        for (Voice voice : Voice.values()) {
+            Document voiceDocument = (Document) basicFormsDocument.get(voice.toString().toLowerCase());
+            for (Tense tense : Tense.values()) {
+                Document tenseDocument = (Document) voiceDocument.get(tense.toString().toLowerCase());
+                for (Mode mode : Mode.values()) {
+                    Document modeDocument = (Document) tenseDocument.get(mode.toString().toLowerCase());
+                    for (CNumber number : CNumber.values()) {
+                        Document numberDocument = (Document) modeDocument.get(number.toString().toLowerCase());
+                        for (Person person : Person.values()) {
+                            String pattern = numberDocument.getString(person.toString().toLowerCase());
+                            VerbForm form = new VerbForm(new ConjugatedForm(person, number), mode, tense, voice);
+                            schema.patterns.put(form, Pattern.fromString(pattern));
                         }
                     }
                 }
             }
-        } catch (Exception e) {
-            throw new LoadingDataException("Could not create SimpleConjugationSchema " + name + ": " + e.getClass().getSimpleName() + " \"" + e.getMessage() + "\"");
         }
 
         return schema;
