87b7aed2d31e93461ee8b9b0d5383a74ad68ee7b
JupiterPi <jupiterpi@outlook.de>
2022-11-24 21:58:48 +0100

    util method for IdentificationResult

--- src/main/java/jupiterpi/vocabulum/core/db/wordbase/IdentificationResult.java
+++ src/main/java/jupiterpi/vocabulum/core/db/wordbase/IdentificationResult.java
@@ -2,6 +2,12 @@ package jupiterpi.vocabulum.core.db.wordbase;
 
 import jupiterpi.vocabulum.core.vocabularies.Vocabulary;
 import jupiterpi.vocabulum.core.vocabularies.VocabularyForm;
+import jupiterpi.vocabulum.core.vocabularies.conjugated.Verb;
+import jupiterpi.vocabulum.core.vocabularies.conjugated.form.VerbForm;
+import jupiterpi.vocabulum.core.vocabularies.declined.adjectives.Adjective;
+import jupiterpi.vocabulum.core.vocabularies.declined.adjectives.AdjectiveForm;
+import jupiterpi.vocabulum.core.vocabularies.declined.nouns.Noun;
+import jupiterpi.vocabulum.core.vocabularies.declined.nouns.NounForm;
 
 import java.util.List;
 
@@ -20,6 +26,21 @@ public class IdentificationResult {
 
     public List<VocabularyForm> getForms() {
         return forms;
+    }
+
+    /* shortcuts */
+
+    public String makePrimaryFoundForm() {
+        if (forms.size() == 0) {
+            return vocabulary.getBaseForm();
+        }
+        VocabularyForm form = forms.get(0);
+        return switch (vocabulary.getKind()) {
+            case NOUN -> ((Noun) vocabulary).makeFormOrDash((NounForm) form);
+            case ADJECTIVE -> ((Adjective) vocabulary).makeFormOrDash((AdjectiveForm) form);
+            case VERB -> ((Verb) vocabulary).makeFormOrDash((VerbForm) form);
+            case INFLEXIBLE -> vocabulary.getBaseForm();
+        };
     }
 
     @Override
