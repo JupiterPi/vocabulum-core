3a1c5ec31d1f2a73dcddf677e7515fd79da7918d
JupiterPi <jupiterpi@outlook.de>
2022-12-19 10:51:16 +0100

    Lecture processes lines using TA

--- src/main/java/jupiterpi/vocabulum/core/db/lectures/Lecture.java
+++ src/main/java/jupiterpi/vocabulum/core/db/lectures/Lecture.java
@@ -1,33 +1,69 @@
 package jupiterpi.vocabulum.core.db.lectures;
 
+import jupiterpi.vocabulum.core.ta.TranslationAssistance;
+import jupiterpi.vocabulum.core.ta.result.TAResult;
 import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Lecture {
     private String name;
-    private String text;
+    private List<String> lines;
 
-    public Lecture(String name, String text) {
+    /* constructors */
+
+    public Lecture(String name, List<String> lines) {
         this.name = name;
-        this.text = text;
+        this.lines = lines;
+    }
+
+    public static Lecture fromTextString(String name, String text) {
+        return new Lecture(
+                name,
+                List.of(text.split("\n"))
+        );
     }
 
     public static Lecture readFromDocument(Document document) {
         return new Lecture(
                 document.getString("name"),
-                document.getString("text")
+                List.of(document.getString("text").split("\n"))
         );
     }
+
+    /* getters, accessors, toString */
 
     public String getName() {
         return name;
     }
 
+    public List<String> getLines() {
+        return lines;
+    }
+
     public String getText() {
-        return text;
+        return String.join("\n", lines);
+    }
+
+    /* process lines */
+
+    private List<TAResult> processedLines = null;
+
+    public List<TAResult> getProcessedLines() {
+        if (processedLines == null) {
+            processedLines = new ArrayList<>();
+            for (String line : lines) {
+                TranslationAssistance ta = new TranslationAssistance(line);
+                processedLines.add(ta.getResult());
+            }
+        }
+        return processedLines;
     }
 
     @Override
     public String toString() {
+        String text = getText();
         String excerpt = text.length() > 30 ? text.substring(0, 30) + "..." : text;
         excerpt = excerpt.replaceAll("\n", " // ");
         return "Lecture{name=" + name + ",text=\"" + excerpt + "\"}";
--- src/test/java/jupiterpi/vocabulum/core/db/lectures/LectureTest.java
+++ src/test/java/jupiterpi/vocabulum/core/db/lectures/LectureTest.java
@@ -1,12 +1,96 @@
 package jupiterpi.vocabulum.core.db.lectures;
 
+import jupiterpi.vocabulum.core.db.Database;
+import jupiterpi.vocabulum.core.db.MockDatabase;
+import jupiterpi.vocabulum.core.db.MockDatabaseSetup;
+import jupiterpi.vocabulum.core.db.MockWordbase;
+import jupiterpi.vocabulum.core.db.wordbase.IdentificationResult;
+import jupiterpi.vocabulum.core.i18n.I18n;
+import jupiterpi.vocabulum.core.ta.result.TAResult;
+import jupiterpi.vocabulum.core.ta.result.TAResultPunctuation;
+import jupiterpi.vocabulum.core.ta.result.TAResultWord;
+import jupiterpi.vocabulum.core.vocabularies.Vocabulary;
+import jupiterpi.vocabulum.core.vocabularies.declined.form.Casus;
+import jupiterpi.vocabulum.core.vocabularies.declined.form.DeclinedForm;
+import jupiterpi.vocabulum.core.vocabularies.declined.form.Gender;
+import jupiterpi.vocabulum.core.vocabularies.declined.form.NNumber;
+import jupiterpi.vocabulum.core.vocabularies.declined.nouns.NounForm;
+import jupiterpi.vocabulum.core.vocabularies.translations.TranslationSequence;
+import org.bson.Document;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockDatabaseSetup.class)
 class LectureTest {
+
+    @Test
+    @DisplayName("(processed) lines")
+    void processedLines() {
+        Vocabulary sampleVocabulary = new Vocabulary(new TranslationSequence(), "test") {
+            @Override
+            public String getBaseForm() {
+                return "word";
+            }
+
+            @Override
+            public String getDefinition(I18n i18n) {
+                return "word, wordis m.";
+            }
+
+            @Override
+            public Kind getKind() {
+                return Kind.NOUN;
+            }
+
+            @Override
+            protected Document generateWordbaseEntrySpecificPart() {
+                return new Document();
+            }
+
+            @Override
+            protected List<String> getAllFormsToString() {
+                return List.of();
+            }
+        };
+
+        ((MockDatabase) Database.get()).injectWordbase(new MockWordbase() {
+            @Override
+            public List<IdentificationResult> identifyWord(String word, boolean partialSearch) {
+                IdentificationResult sampleIdentificationResult = new IdentificationResult(sampleVocabulary, List.of(new NounForm(new DeclinedForm(Casus.NOM, NNumber.SG, Gender.MASC))));
+                if (List.of("asinus", "stat", "vocat").contains(word)) return List.of(sampleIdentificationResult);
+                else return List.of();
+            }
+        });
+
+        Lecture lecture = Lecture.fromTextString("name", "Asinus stat.\nQuintus vocat.");
+        assertAll(
+            () -> assertEquals(List.of("Asinus stat.", "Quintus vocat."), lecture.getLines()),
+            () -> assertAll(
+                () -> {
+                    TAResult result = lecture.getProcessedLines().get(0);
+                    assertAll(
+                        () -> assertEquals(1, ((TAResultWord) result.getItems().get(0)).getPossibleWords().size()),
+                        () -> assertEquals(1, ((TAResultWord) result.getItems().get(1)).getPossibleWords().size()),
+                            () -> assertTrue(result.getItems().get(2) instanceof TAResultPunctuation)
+                    );
+                },
+                () -> {
+                    TAResult result = lecture.getProcessedLines().get(1);
+                    assertAll(
+                        () -> assertEquals(0, ((TAResultWord) result.getItems().get(0)).getPossibleWords().size()),
+                        () -> assertEquals(1, ((TAResultWord) result.getItems().get(1)).getPossibleWords().size()),
+                        () -> assertTrue(result.getItems().get(2) instanceof TAResultPunctuation)
+                    );
+                }
+            )
+        );
+    }
 
     @Nested
     @DisplayName("toString()")
@@ -15,21 +99,21 @@ class LectureTest {
         @Test
         @DisplayName("short")
         void shortText() {
-            Lecture l = new Lecture("Name", "This is short");
+            Lecture l = Lecture.fromTextString("Name", "This is short");
             assertEquals("Lecture{name=Name,text=\"This is short\"}", l.toString());
         }
 
         @Test
         @DisplayName("shortened")
         void shortened() {
-            Lecture l = new Lecture("Name", "This is some example text to check if this works");
+            Lecture l = Lecture.fromTextString("Name", "This is some example text to check if this works");
             assertEquals("Lecture{name=Name,text=\"This is some example text to c...\"}", l.toString());
         }
 
         @Test
         @DisplayName("with line breaks")
         void withLineBreaks() {
-            Lecture l = new Lecture("Name", "This is some example text.\nIt will check if this works.");
+            Lecture l = Lecture.fromTextString("Name", "This is some example text.\nIt will check if this works.");
             assertEquals("Lecture{name=Name,text=\"This is some example text. // It ...\"}", l.toString());
         }
 
